025a14053ee5dcccba1044320a1c7c71adfeece7
reza-soufi <reza.soufi2014@gmail.com>
2025-09-21 16:08:07 +0330

    create project and add header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <ul className="flex gap-4">
+        <li className="hidden sm:block">
+          <Link href={"/sign-in"}>Sign in</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/favorites"}>Favorites</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href={"/about"}>About</Link>
+        </li>
+      </ul>
+      <div className="flex items-center gap-4">
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
