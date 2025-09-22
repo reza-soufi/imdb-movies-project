2b702de559c91b56bb8a75d33cae86e4f098eed4
reza-soufi <reza.soufi2014@gmail.com>
2025-09-22 12:29:56 +0330

    add theme functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/layout/Header';
 import { ReactNode } from 'react';
 import ThemeProviderContent from '@/components/theme/ThemeProvider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
